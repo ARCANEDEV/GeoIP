bb24ab929b91db2a823d9a209f75ee235f7b506c
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-01-19 16:56:54 +0000

    Seperate LaravelTestCase and PHPUnit TestCase

---
+++ tests/LaravelTestCase.php
@@ -0,0 +1,76 @@
+<?php namespace Arcanedev\GeoIP\Tests;
+
+abstract class LaravelTestCase extends \Orchestra\Testbench\TestCase
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    protected function getPackageProviders()
+    {
+        return [
+            'Arcanedev\GeoIP\ServiceProvider'
+        ];
+    }
+
+    protected function getPackageAliases()
+    {
+        return [
+            'GeoIP' => 'Arcanedev\GeoIP\Facade'
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['path.base']     = realpath(__DIR__ . '/../src');
+        $app['path.database'] = realpath(__DIR__ . '/database');
+
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        $this->setPackageConfig($app);
+    }
+
+    /**
+     * Define Package config
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    private function setPackageConfig($app)
+    {
+        $app['config']->set('geo-ip::config', [
+            'connection' => 'testbench',
+            'prefix'     => 'geo_',
+            'table'      => [
+                'nations'   => 'nations',
+                'countries' => 'countries',
+            ],
+            'dump'       => $app['path.database'] . '/geo-db.sql',
+        ]);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php namespace Arcanedev\GeoIP\Tests;
 
-abstract class TestCase extends \Orchestra\Testbench\TestCase
+abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
@@ -14,63 +14,5 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     public function tearDown()
     {
         parent::tearDown();
-    }
-
-    /* ------------------------------------------------------------------------------------------------
-     |  Other Functions
-     | ------------------------------------------------------------------------------------------------
-     */
-    protected function getPackageProviders()
-    {
-        return [
-            'Arcanedev\GeoIP\ServiceProvider'
-        ];
-    }
-
-    protected function getPackageAliases()
-    {
-        return [
-            'GeoIP' => 'Arcanedev\GeoIP\Facade'
-        ];
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['path.base']     = __DIR__ . '/../src';
-        $app['path.database'] = $app['path.base'];
-
-        $app['config']->set('database.default', 'testbench');
-        $app['config']->set('database.connections.testbench', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ]);
-
-        $this->setPackageConfig($app);
-    }
-
-    /**
-     * Define Package config
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    private function setPackageConfig($app)
-    {
-        $app['config']->set('geo-ip::config', [
-            'connection' => 'testbench',
-            'prefix'     => 'geo_',
-            'table'      => [
-                'nations'   => 'nations',
-                'countries' => 'countries',
-            ],
-            'dump'       => $app['path.database'] . '/geo-db.sql'
-        ]);
     }
 }
