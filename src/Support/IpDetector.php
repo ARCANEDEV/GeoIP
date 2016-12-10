4d013a8e2ca336939e8dd4f84c021de51e233aad
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-12-10 16:30:29 +0000

    Adding the support classes

---
+++ src/Support/IpDetector.php
@@ -0,0 +1,52 @@
+<?php namespace Arcanedev\GeoIP\Support;
+
+use Arcanedev\GeoIP\Support\IpValidator;
+
+/**
+ * Class     IpDetector
+ *
+ * @package  Arcanedev\GeoIP\Support
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class IpDetector
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @var string */
+    protected static $default = '127.0.0.0';
+
+    /** @var array */
+    protected static $remotes = [
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_FORWARDED',
+        'REMOTE_ADDR',
+        'HTTP_X_CLUSTER_CLIENT_IP',
+    ];
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Detect the IP address.
+     *
+     * @return string
+     */
+    public static function detect()
+    {
+        foreach (static::$remotes as $remote) {
+            if ($address = getenv($remote)) {
+                foreach (explode(',', $address) as $ipAddress) {
+                    if (IpValidator::validate($ipAddress)) return $ipAddress;
+                }
+            }
+        }
+
+        return static::$default;
+    }
+}
